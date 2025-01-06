035f5350675388b70b1785b22d5427c5e2ceecd0
lianlevwe <lianlevwe@gmail.com>
2025-01-06 12:09:33 +0200

    Finish User

--- User.java
+++ User.java
@@ -44,33 +44,48 @@
     /** If this user follows the given name, returns true; otherwise returns false. */
     public boolean follows(String name) {
         //// Replace the following statement with your code
+        for (int i = 0; i < follows.length; i++) {
+            if (follows[i] == name)
+                return true;
+        }
         return false;
     }
     /** Makes this user follow the given name. If successful, returns true. 
      *  If this user already follows the given name, or if the follows list is full, does nothing and returns false; */
     public boolean addFollowee(String name) {
         //// Replace the following statement with your code
-        return false;
+        if (fCount == maxfCount) return false;
+        if (follows(name)) return false;
+
+        follows[fCount] = name;
+        fCount++;
+        return true;
     }
 
     /** Removes the given name from the follows list of this user. If successful, returns true.
      *  If the name is not in the list, does nothing and returns false. */
     public boolean removeFollowee(String name) {
         //// Replace the following statement with your code
-        return false;
+        if (!follows(name)) return false;
+        for (int i = 0; i < follows.length; i++) {
+            if (follows[i] == name) follows[i] = null;
+        }
+        fCount--;
+        return true;
     }
 
     /** Counts the number of users that both this user and the other user follow.
     /*  Notice: This is the size of the intersection of the two follows lists. */
     public int countMutual(User other) {
          //// Replace the following statement with your code
-        return 0;
+        return (fCount + other.fCount);
     }
 
     /** Checks is this user is a friend of the other user.
      *  (if two users follow each other, they are said to be "friends.") */
     public boolean isFriendOf(User other) {
         //// Replace the following statement with your code
+        if (follows(other.name) && other.follows(name)) return true;
         return false;
     }
     /** Returns this user's name, and the names that s/he follows. */
